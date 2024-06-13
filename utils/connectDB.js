9d31adf54143359be5d3aa1a4336b67d3fec2854
shredam <abdosheredam@gmail.com>
2024-06-13 21:21:27 +0300

    edit DB

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -6,12 +6,12 @@ dotenv.config();
 
 const connectDB = catchAsyncErrors(async (req, res, next) => {
   let connectDBLink;
-  // if (process.env.NODE_ENV === 'development') {
-  //   connectDBLink = await mongoose.connect(`mongodb://127.0.0.1:27017/social`);
-  // } else if (process.env.NODE_ENV === 'production') {
-  //   connectDBLink = await mongoose.connect(process.env.Mongo_Atlas);
-  // }
-  connectDBLink = await mongoose.connect(process.env.Mongo_Atlas);
+  if (process.env.NODE_ENV === 'development') {
+    connectDBLink = await mongoose.connect(process.env.Mongo_Atlas);
+  } else if (process.env.NODE_ENV === 'production') {
+    connectDBLink = await mongoose.connect(process.env.Mongo_Atlas);
+  }
+  // connectDBLink = await mongoose.connect(process.env.Mongo_Atlas);
 
   console.log(`db okay ${connectDBLink.connection.host}`);
 });
