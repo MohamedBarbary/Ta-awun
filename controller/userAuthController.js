400a274fdff84c97a6c0843b04838d84e9a5b327
Mohamed Nabil <mohamedalbarbary0@gamil.com>
2024-06-21 19:53:42 +0300

    sign up update

--- controller/userAuthController.js
+++ controller/userAuthController.js
@@ -71,6 +71,8 @@ exports.signUp = catchAsyncError(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    role: req.body.role,
+    user: req.body.user,
   });
   if (user.userType === 'user') {
     await prepareAndSendVerificationEmailUser(user, req);
